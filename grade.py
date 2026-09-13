a0d8ef5a56ebdaae9a4ee49b866477faa5316654
May Thu Myint <mayelsa827@gmail.com>
2026-09-13 21:19:28 +0630

    Add total mark

--- grade.py
+++ grade.py
@@ -3,6 +3,7 @@ mark1 = float(input("Enter first subject mark: "))
 mark2 = float(input("Enter second subject mark: "))
 
 average = (mark1 + mark2) / 2
-
+total = mark1 + mark2
 print("\nStudent Name:", name)
 print("Average Mark:", average)
+print("Total Mark:", total)
